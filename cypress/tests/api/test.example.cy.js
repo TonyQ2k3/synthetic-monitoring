127c7f4e25fedc7416f33d98fb1f92dc5247d0c8
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-11-08 10:12:50 +0700

    Update example test

---
+++ cypress/tests/api/test.example.cy.js
@@ -0,0 +1,10 @@
+context("GET /post", () => {
+    it("gets a list of all jsonplaceholder posts", () => {
+        cy.request({
+            method: "GET", 
+            url: "https://jsonplaceholder.typicode.com/posts"
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+})
